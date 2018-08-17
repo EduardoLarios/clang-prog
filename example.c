77f2c0cf0033c047a0dfdb35f453b05eacfeef67
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2018-08-17 16:09:21 -0500

    Added some pointer examples to example.c

--- example.c
+++ example.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct {
+    double *data;
+    unsigned int nrows, ncols;
+} Matrix;
 
 int main()
 {
-    int i, j, rows;
+    // int i, j;
 
-    printf("Enter number of rows: ");
-    scanf("%d",&rows);
+    // // printf("Enter number of rows: ");
+    // // scanf("%d",&rows);
+    // int rows = 7;
 
-    for(i=1; i<=rows; ++i)
-    {
-        for(j=1; j<=i; ++j)
-        {
-            printf("* ");
-        }
-        printf("\n");
-    }
+    // for(i=1; i<=rows; ++i)
+    // {
+    //     for(j=1; j<=i; ++j)
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    Matrix *ptr;
+    ptr = (Matrix *) malloc(sizeof(Matrix));
+
+    printf("%i\n", (int)ptr);
+    printf("%i\n", sizeof(Matrix));
+    printf("%i\n", sizeof(double));
+    printf("%i\n", sizeof(int));
+
     return 0;
 }
